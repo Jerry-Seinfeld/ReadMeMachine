292b726c724314fc01d6490716c837b4424c4a43
Andy <andrewryzen@gmail.com>
2020-03-22 16:17:26 -0400

    ReadMe and Javascript in progress

---
+++ index.js
@@ -0,0 +1,106 @@
+const fs = require('fs');
+const axios = require("axios");
+const inquirer = require('inquirer');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile)
+
+function repoQuestions() {
+return inquirer.prompt ([
+    
+    {type: "input",
+        name: "username",
+        message: "What is your Github Username?"},
+    {type: "input",
+        name: "title",
+        message: "The Title of your project?"},
+    {type: "input",
+        name: "description",
+        message: "Describe your project"},
+    {type: "input",
+        name: "license",
+        message: "What kind of license should your project have?"},
+    {type: "input",
+        name: "installation",
+        message: "What command should be run to install the necessary dependecies?"},
+    {type: "input",
+        name: "tests",
+        message: "Write a desired command that will run the tests"},
+    {type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"},
+    {type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?"},
+    
+])
+};
+
+var getName = (data) => {
+    var queryUrl = `https://api.github.com/users/${data.username}`;
+    return axios.get(queryUrl)
+}
+
+
+const generateMarkdown = (data, image, email) => {
+    console.log(data)
+    return `
+# ${data.title}
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)]
+
+## Description
+  ${data.description}
+
+  ## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+  
+## Installation
+To install run in the terminal:
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.usage} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+Run the command:
+    ${data.tests}
+
+    ## Questions
+<img src="${image}" alt="avatar" style="border-radius: 16px" width="30"/>
+For questions or concerns contact me at
+[${data.username}]${email}
+  `
+};
+
+
+
+  async function init() {
+  console.log("Generating README")
+  try {
+     
+    const data = await repoQuestions();
+
+    const res =  await getName(data);
+
+    const md =  generateMarkdown(data,res.data.avatar_url,res.data.html_url);
+
+    await writeFileAsync("README.md", md);
+
+    console.log("Successfully wrote to README.md");
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+init();
